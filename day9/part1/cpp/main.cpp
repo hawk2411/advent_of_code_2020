01c97ce7b9d0a7626aa3375405a3e92d49093a0b
hawk2411 <wagner-harry@web.de>
2021-11-23 20:12:24 +0100

    Started with day 9

--- day9/part1/cpp/main.cpp
+++ day9/part1/cpp/main.cpp
@@ -3,11 +3,36 @@
 #include <string>
 #include <algorithm>
 #include <memory>
+#include <vector>
 
 int main() {
     std::ifstream input_data("./input_data.txt");
     if (!input_data.is_open()) { return 1; }
 
-    std::cerr << "Found no solution" << std::endl;
+    std::string line;
+    std::vector<unsigned int > data;
+    const unsigned int preamble = 5;
 
+    while(std::getline(input_data, line)) {
+        data.push_back(std::strtol(line.c_str(), nullptr, 10));
+    }
+
+    for (auto current = data.begin() + preamble; current != data.end(); current++) {
+        bool found = false;
+        std::sort(data.begin(), current);
+        for (auto t = current - preamble-1; t != current; t++) {
+            auto result = std::find_if(std::next(t), std::prev(current),
+                                       [&current, &t](unsigned int digit){return ((*t + digit) == *current);});
+
+            if(result != std::prev(current) || (*std::prev(current) + *t == *current)) {
+                found = true;
+                break;
+            }
+        }
+        if(!found) {
+            std::cout << "Result is: " << *current << std::endl;
+            break;
+        }
+    }
+    return 0;
 }
